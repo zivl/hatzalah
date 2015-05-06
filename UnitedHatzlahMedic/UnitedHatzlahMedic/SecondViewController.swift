851dbf1134269f30116fc45cd7bee963365f333f
Ziv Levy <levi.ziv@gmail.com>
2015-05-06 14:42:25 +0300

    initial check-in for iOS version

---
+++ UnitedHatzlahMedic/UnitedHatzlahMedic/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  UnitedHatzlahMedic
+//
+//  Created by Ziv Levy on 5/2/15.
+//  Copyright (c) 2015 Ziv Levy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
